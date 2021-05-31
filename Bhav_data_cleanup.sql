a749a08a9786e3f78e2fe663bcad3fa71f013ea7
Thiyanesh <Thiyaneshk@gmail.com>
2021-05-31 16:46:56 +0530

    Update Bhav_data_cleanup.sql

--- Bhav_data_cleanup.sql
+++ Bhav_data_cleanup.sql
@@ -25,6 +25,28 @@ CREATE TABLE bhav
 -- delete records which is not EQ
 
 
+select * from bhav
+
+select * from (
+select symbol,week,traded_qnt,DENSE_RANK() OVER (PARTITION BY symbol order by traded_qnt desc) as rankk from (
+select symbol,trunc(date1, 'IW') as week, sum(TTL_TRD_QNTY)  AS traded_qnt
+from bhav --where symbol ='TTKHLTCARE'
+group by trunc(date1, 'IW') ,symbol  ) ranking_tab ) temp_tab where week   between '01-May-21' and '31-May-21' and rankk =1;
+
+
+select * from (
+select symbol,week,turnover,DENSE_RANK() OVER (PARTITION BY symbol order by turnover desc) as rankk from (
+select symbol,trunc(date1, 'IW') as week, sum(TURNOVER_LACS)  AS turnover
+from bhav --where symbol ='TTKHLTCARE'
+group by trunc(date1, 'IW') ,symbol  ) ranking_tab ) temp_tab where week   between '24-MAY-21' and '31-May-21' and rankk =1 order by turnover desc
+
+
+
+
+select * from (
+select symbol,date1, TURNOVER_LACS   ,DENSE_RANK() OVER (PARTITION BY symbol order by TURNOVER_LACS desc) as rankk from bhav   
+)where date1 between '27-MAY-21' and '31-May-21' and rankk =1 order by TURNOVER_LACS desc
+
 delete from bhav_master where trim(series) <> 'EQ';
 commit;
 update bhav_master set SYMBOL= trim(SYMBOL) , SERIES=trim(SERIES),  DELIV_QTY=trim(DELIV_QTY) , DELIV_PER=trim(DELIV_PER);
